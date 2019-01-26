4722197b849116a64b0a275f272128aecbf872e1
= <=>
2019-01-26 13:30:45 +0000

    Added user, server update, passport and mongoURI

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const db = require('./config/keys').mongoURI;
 
 // Route Requires
 const userRoutes = require('./routes/api/user/userRoutes');
 
-
 // Mongoose set up
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDb Connected'))
+    .catch((err => console.log(err)));
 
 // Setting up port and initialising the server
 const app = express();
@@ -18,13 +22,14 @@ app.listen(port, () => {
 });
 
 // Setting up the body parser
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Passport config
+require('./config/passport')(passport);
 
 // Use Routes
 app.use('api/users', userRoutes);
 
 // Default test route
-app.get('/test', (req, res) => res.json({msg: 'Test'}));
+app.get('/test', (req, res) => res.json({ msg: 'Test' }));
