9422e73547972411d37a38512458d798d97cfddd
= <=>
2019-02-03 23:35:12 +0000

    Added the handle and userId search

--- routes/api/profile/handle.js
+++ routes/api/profile/handle.js
@@ -6,13 +6,22 @@ const router = express.Router();
 // @route   api/profile/handle/test
 // @desc    Default test route
 // @access  Public
-router.get('/test', (req, res) => res.json({msg: 'ProfileRoutes Handle Test'}));
+router.get('/test', (req, res) => res.json({ msg: 'ProfileRoutes Handle Test' }));
 
 // @route   api/profile/handle/{handle}
 // @desc    Gets user by their handle
 // @access  Public
 router.get('/:handle', (req, res) => {
-    res.json({msg: 'Get By Handle'});
+    const errors = {};
+    Profile.findOne({ handle: req.params.handle })
+        .populate('user', ['name', 'email', 'avatar'])
+        .then(profile => {
+            if (!profile) {
+                errors.NoProfile = 'No profile found';
+                res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
 })
 
 module.exports = router;
--- routes/api/profile/userId.js
+++ routes/api/profile/userId.js
@@ -1,17 +1,27 @@
 // Requires
 const express = require('express');
+const Profile = require('../../../models/ProfileModel');
 const router = express.Router();
 
 // @route   api/profile/user/test
 // @desc    Default test route
 // @access  Public
-router.get('/test', (req, res) => res.json({msg: 'ProfileRoutes User Test'}));
+router.get('/test', (req, res) => res.json({ msg: 'ProfileRoutes User Test' }));
 
 // @route   api/profile/user/{id}
 // @desc    Gets a user by their id
 // @access  Public
 router.get('/:id', (req, res) => {
-    res.json({msg: 'Get By Id'});
+    const errors = {};
+    Profile.findOne({ user: req.params.id })
+        .populate('user', ['name', 'email', 'avatar'])
+        .then(profile => {
+            if (!profile) {
+                errors.NoProfile = 'No Profile Found';
+                res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
 });
 
 module.exports = router;
